1c7359139d2b6dd8e43bbf003ef22805410a570c
davhe786 <davhe786@student.liu.se>
2025-09-17 11:33:07 +0200

    bullet collision fixed

--- bullet.gd
+++ bullet.gd
@@ -4,3 +4,8 @@ var velocity: Vector2 = Vector2.ZERO
 
 func _physics_process(delta: float) -> void:
 	position += velocity * delta
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "Enemy":
+		queue_free()
